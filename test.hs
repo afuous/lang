67fcf9207daf0d07cd7fcd0755fc26fddd735a50
afuous <benheller42@gmail.com>
2016-07-04 16:43:49 -0700

    Add more parsing stuff

--- test.hs
+++ test.hs
@@ -1,7 +1,10 @@
 module Test where
 
-import           Control.Monad.Trans.Maybe
+import           Control.Monad (msum)
 import           Control.Monad.State
+import           Control.Monad.Trans.Maybe
+import           Data.List (elemIndex)
+import           Data.Maybe (isJust)
 import           Types
 
 
@@ -16,21 +19,35 @@ getNotEmpty = do
         then liftMaybe Nothing
         else return xs
 
-expectToken :: Token -> Parser ()
-expectToken token = do
+getToken :: Parser Token
+getToken = do
     tokens <- getNotEmpty
     modify $ tail
-    if head tokens == token
+    return $ head tokens
+
+expectToken :: Token -> Parser ()
+expectToken t = do
+    token <- getToken
+    if token == t
         then return ()
         else liftMaybe Nothing
 
 expectIdent :: Parser Ident
 expectIdent = do
-    tokens <- get
-    modify $ tail
-    case head tokens of
+    token <- getToken
+    case token of
         (TIdent ident) -> return ident
         _ -> liftMaybe Nothing
+
+expectExprUntil :: Token -> Parser Expr
+expectExprUntil token = do
+    tokens <- get
+    index <- liftMaybe $ token `elemIndex` tokens
+    modify $ drop (index + 1)
+    liftMaybe $ runParser (take index tokens) parseExpr
+
+parseExpr :: Parser Expr
+parseExpr = do
 
 parseInput :: Parser Instr
 parseInput = do
@@ -39,7 +56,21 @@ parseInput = do
     expectToken TSemicolon
     return $ Input ident
 
+instrParsers :: [Parser Instr]
+instrParsers = [parseInput]
+
+runParser :: [Token] -> Parser a -> (Maybe a, [Token])
+runParser tokens parser = runState (runMaybeT parser) tokens
+
+parse :: [Token] -> Maybe Block
+parse [] = Just []
+parse tokens =
+    let parsed = map (runParser tokens) instrParsers in
+    case filter (\(v, tokens) -> isJust v) parsed of
+        [] -> Nothing
+        (((Just instr), ts):_) -> fmap (instr:) (parse ts)
+
 main :: IO ()
 main = do
-    print $ runState (runMaybeT parseInput) [TInput, TIdent (Ident "hello"), TSemicolon]
+    print $ parse $ [TInput, TIdent (Ident "hello"), TSemicolon, TInput, TIdent (Ident "a"), TSemicolon]
 
