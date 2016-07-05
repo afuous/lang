f89235aace339d8d760f1cb0f3be2460310a9f2a
afuous <benheller42@gmail.com>
2016-07-04 17:12:34 -0700

    Replace old parser with new parser

--- Parser.hs
+++ Parser.hs
@@ -1,42 +1,51 @@
 module Parser (parseBlock) where
 
 import           Control.Monad (msum)
+import           Control.Monad.State
+import           Control.Monad.Trans.Maybe
 import           Data.List (elemIndex)
-import           Data.Maybe (fromJust)
+import           Data.Maybe (fromJust, isJust)
 import           Types
 
-parseBlock :: [Token] -> Block
-parseBlock [] = []
-parseBlock (TOutput:xs) =
-    let count = findToken TSemicolon xs
-    in (Output $ parseExpr $ take count xs) : parseBlock (drop (count + 1) xs)
-parseBlock (TInput:(TIdent ident):xs) =
-    (Input ident) : parseBlock (drop 1 xs)
-parseBlock ((TIdent ident):TEquals:xs) =
-    let count = findToken TSemicolon xs
-    in (Assignment ident $ parseExpr $ take count xs) : parseBlock (drop (count + 1) xs)
-parseBlock (TIf:xs) =
-    let count1 = findToken TLeftCurl xs
-        count2 = findMatchingCurl $ drop (count1 + 1) xs
-    in (IfBlock (parseExpr $ take count1 xs) (parseBlock $ take count2 $ drop (count1 + 1) xs)) : parseBlock (drop (count1 + count2 + 2) xs)
-parseBlock (TWhile:xs) =
-    let count1 = findToken TLeftCurl xs
-        count2 = findMatchingCurl $ drop (count1 + 1) xs
-    in (WhileBlock (parseExpr $ take count1 xs) (parseBlock $ take count2 $ drop (count1 + 1) xs)) : parseBlock (drop (count1 + count2 + 2) xs)
-parseBlock _ = error "syntax error"
 
-findToken :: Token -> [Token] -> Int
-findToken token tokens = fromJust $ token `elemIndex` tokens
+type Parser a = MaybeT (State [Token]) a
 
-findMatchingCurl :: [Token] -> Int
-findMatchingCurl xs = walk xs 1
-  where
-    walk :: [Token] -> Int -> Int
-    walk [] _ = error "mismatched brackets"
-    walk (TLeftCurl:xs) n = 1 + walk xs (n + 1)
-    walk (TRightCurl:_) 1 = 0 -- exclusive of last }
-    walk (TRightCurl:xs) n = 1 + walk xs (n - 1)
-    walk (_:xs) n = 1 + walk xs n
+liftMaybe = MaybeT . return
+
+getNotEmpty :: Parser [Token]
+getNotEmpty = do
+    xs <- get
+    if null xs
+        then liftMaybe Nothing
+        else return xs
+
+getToken :: Parser Token
+getToken = do
+    tokens <- getNotEmpty
+    modify $ tail
+    return $ head tokens
+
+expectToken :: Token -> Parser ()
+expectToken t = do
+    token <- getToken
+    if token == t
+        then return ()
+        else liftMaybe Nothing
+
+expectIdent :: Parser Ident
+expectIdent = do
+    token <- getToken
+    case token of
+        (TIdent ident) -> return ident
+        _ -> liftMaybe Nothing
+
+expectExprUntil :: Token -> Parser Expr
+expectExprUntil token = do
+    tokens <- get
+    index <- liftMaybe $ token `elemIndex` tokens
+    modify $ drop (index + 1)
+    expr <- liftMaybe $ parseExpr (take index tokens)
+    return expr
 
 operators :: [[Char]]
 operators = [ ['+', '-']
@@ -44,10 +53,11 @@ operators = [ ['+', '-']
             , ['/']
             ]
 
-parseExpr :: [Token] -> Expr
-parseExpr [] = error "empty expression"
-parseExpr [TLiteral value] = Constant value
-parseExpr [TIdent ident] = Variable ident
+-- I give up
+parseExpr :: [Token] -> Maybe Expr
+parseExpr [] = Nothing
+parseExpr [TLiteral value] = Just $ Constant value
+parseExpr [TIdent ident] = Just $ Variable ident
 parseExpr xs =
     let rev = reverse xs
     in case findOperator rev of
@@ -57,7 +67,7 @@ parseExpr xs =
         Just pos -> let right = reverse $ take pos rev
                         left = reverse $ drop (pos + 1) rev
                         (TOperator char) = rev !! pos
-                    in Operator char (parseExpr left) (parseExpr right)
+                    in Just $ Operator char (fromJust $ parseExpr left) (fromJust $ parseExpr right)
   where
 
     findOperator :: [Token] -> Maybe Int
@@ -65,8 +75,43 @@ parseExpr xs =
 
     walk :: [Token] -> Int -> [Char] -> Maybe Int
     walk [] _ _ = Nothing
-    walk _ n _ | n < 0 = error "mismatched parentheses"
+    walk _ n _ | n < 0 = Nothing
     walk (TRightParen:xs) n ops = fmap (+1) $ walk xs (n + 1) ops
     walk (TLeftParen:xs) n ops = fmap (+1) $ walk xs (n - 1) ops
     walk ((TOperator op):xs) 0 ops | op `elem` ops = Just 0
     walk (x:xs) n ops = fmap (+1) $ walk xs n ops
+
+parseInput :: Parser Instr
+parseInput = do
+    expectToken TInput
+    ident <- expectIdent
+    expectToken TSemicolon
+    return $ Input ident
+
+parseOutput :: Parser Instr
+parseOutput = do
+    expectToken TOutput
+    expr <- expectExprUntil TSemicolon
+    return $ Output expr
+
+parseAssignment :: Parser Instr
+parseAssignment = do
+    ident <- expectIdent
+    expectToken TEquals
+    expr <- expectExprUntil TSemicolon
+    return $ Assignment ident expr
+
+instrParsers :: [Parser Instr]
+instrParsers = [parseInput, parseOutput, parseAssignment]
+
+runParser :: [Token] -> Parser a -> (Maybe a, [Token])
+runParser tokens parser = runState (runMaybeT parser) tokens
+
+parseBlock :: [Token] -> Block
+parseBlock [] = []
+parseBlock tokens =
+    let parsed = map (runParser tokens) instrParsers in
+    case filter (\(v, tokens) -> isJust v) parsed of
+        [] -> error "this is bad error handling"
+        (((Just instr), ts):_) -> instr : parseBlock ts
+
