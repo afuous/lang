cb3d52f4b5cc51f580995f3ba4dce2dbfef851b1
afuous <benheller42@gmail.com>
2016-07-05 02:57:50 -0700

    Add parsing for input, output, and some expressions

--- Parser.hs
+++ Parser.hs
@@ -1,6 +1,68 @@
 module Parser (parseCode) where
 
+import           Control.Monad (void)
 import           Text.Parsec
+import           Text.Parsec.Char
+import           Text.Parsec.String (Parser)
 import           Types
 
-parseCode _ = []
+parseCode :: String -> Block
+parseCode s = case parse (many $ inputInstr <|> outputInstr) "mistake" s of
+    Right instrs -> instrs
+    Left err     -> error $ show err
+
+whitespace :: Parser ()
+whitespace = void $ many $ oneOf "\n\t "
+
+lexeme :: Parser a -> Parser a
+lexeme p = p <* whitespace
+
+literal :: Parser Value
+literal = lexeme $ Value <$> read <$> many1 digit
+
+identifier :: Parser Ident
+identifier = lexeme $ Ident <$> many1 letter
+
+input :: Parser ()
+input = void $ lexeme $ string "input"
+
+output :: Parser ()
+output = void $ lexeme $ string "output"
+
+semicolon :: Parser ()
+semicolon = void $ lexeme $ char ';'
+
+inputInstr :: Parser Instr
+inputInstr = do
+    input
+    ident <- identifier
+    semicolon
+    return $ Input ident
+
+outputInstr :: Parser Instr
+outputInstr = do
+    output
+    expr <- expression
+    semicolon
+    return $ Output expr
+
+parens :: Parser a -> Parser a
+parens p = void (lexeme $ char '(') *> p <* void (lexeme $ char ')')
+
+operator :: Parser Expr
+operator = do
+    a <- termTest
+    chr <- oneOf "+-*/"
+    b <- expression
+    return $ Operator chr a b
+
+term :: Parser Expr -> Parser Expr
+term expr =   Constant <$> literal
+          <|> Variable <$> identifier
+          <|> parens expr
+
+termTest = term termTest
+
+expression :: Parser Expr
+expression =   try operator
+           <|> termTest
