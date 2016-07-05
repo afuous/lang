e2d0e75dbd5d1f7e3f51adf547a80d6a8867f582
afuous <benheller42@gmail.com>
2016-07-04 17:07:13 -0700

    Finish some features of new parser

--- test.hs
+++ test.hs
@@ -4,7 +4,7 @@ import           Control.Monad (msum)
 import           Control.Monad.State
 import           Control.Monad.Trans.Maybe
 import           Data.List (elemIndex)
-import           Data.Maybe (isJust)
+import           Data.Maybe (fromJust, isJust)
 import           Types
 
 
@@ -44,10 +44,42 @@ expectExprUntil token = do
     tokens <- get
     index <- liftMaybe $ token `elemIndex` tokens
     modify $ drop (index + 1)
-    liftMaybe $ runParser (take index tokens) parseExpr
+    expr <- liftMaybe $ parseExpr (take index tokens)
+    return expr
 
-parseExpr :: Parser Expr
-parseExpr = do
+operators :: [[Char]]
+operators = [ ['+', '-']
+            , ['*', '/', '%']
+            , ['/']
+            ]
+
+-- I give up
+parseExpr :: [Token] -> Maybe Expr
+parseExpr [] = Nothing
+parseExpr [TLiteral value] = Just $ Constant value
+parseExpr [TIdent ident] = Just $ Variable ident
+parseExpr xs =
+    let rev = reverse xs
+    in case findOperator rev of
+        Nothing -> if head xs == TLeftParen && last xs == TRightParen
+            then parseExpr $ init $ tail xs
+            else error "invalid expression"
+        Just pos -> let right = reverse $ take pos rev
+                        left = reverse $ drop (pos + 1) rev
+                        (TOperator char) = rev !! pos
+                    in Just $ Operator char (fromJust $ parseExpr left) (fromJust $ parseExpr right)
+  where
+
+    findOperator :: [Token] -> Maybe Int
+    findOperator rev = msum $ map (walk rev 0) operators
+
+    walk :: [Token] -> Int -> [Char] -> Maybe Int
+    walk [] _ _ = Nothing
+    walk _ n _ | n < 0 = Nothing
+    walk (TRightParen:xs) n ops = fmap (+1) $ walk xs (n + 1) ops
+    walk (TLeftParen:xs) n ops = fmap (+1) $ walk xs (n - 1) ops
+    walk ((TOperator op):xs) 0 ops | op `elem` ops = Just 0
+    walk (x:xs) n ops = fmap (+1) $ walk xs n ops
 
 parseInput :: Parser Instr
 parseInput = do
@@ -55,6 +87,19 @@ parseInput = do
     ident <- expectIdent
     expectToken TSemicolon
     return $ Input ident
+
+parseOutput :: Parser Instr
+parseOutput = do
+    expectToken TOutput
+    expr <- expectExprUntil TSemicolon
+    return $ Output expr
+
+parseAssignment :: Parser Instr
+parseAssignment = do
+    ident <- expectIdent
+    expectToken TEquals
+    expr <- expectExprUntil TSemicolon
+    return $ Assignment ident expr
 
 instrParsers :: [Parser Instr]
 instrParsers = [parseInput]
